a3b696011df537f5613dc6c9eabac0fcae978f4d
michballard <michballard@gmail.com>
2014-08-25 11:20:54 +0100

    Formatting changes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,19 +9,19 @@ describe Airport do
 	let(:test_airport) { Airport.new(capacity: 6) }
 
 	def landing_a_plane
-		allow(airport).to receive(:weather).and_return(1, 2, 3)
+		allow(airport).to receive(:weather).and_return(1,3)
 		airport.accept(plane)
 		plane.land!
 	end
 
 	def taking_off_a_plane
-		allow(airport).to receive(:weather).and_return(1, 2, 3)
+		allow(airport).to receive(:weather).and_return(1,3)
 		airport.release(plane)
 		plane.take_off!
 	end
 
 	def fill_airport(airport_to_fill)
-		allow(airport_to_fill).to receive(:weather).and_return(1, 2, 3)
+		allow(airport_to_fill).to receive(:weather).and_return(1,3)
 		airport_to_fill.capacity.times { airport_to_fill.accept(Plane.new.land!) }
 	end
 
@@ -81,7 +81,7 @@ describe Airport do
 			flying_planes = []			
 			while test_airport.plane_count > 0
 				test_airport.planes.each do |plane|
-					allow(airport).to receive(:weather).and_return(1, 2, 3, 4, 5)
+					allow(airport).to receive(:weather).and_return(1,5)
 					test_airport.release(plane)
 					plane.take_off!
 					flying_planes << plane
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -9,24 +9,24 @@ describe Weather do
 
 	context 'weather conditions' do 
 		it "knows when it's sunny" do 
-			allow(airport).to receive(:weather).and_return(1, 2, 3)
+			allow(airport).to receive(:weather).and_return(1,3)
 			expect(airport).to be_sunny
 		end
 
 		it "knows when it's stormy" do 
-			allow(airport).to receive(:weather).and_return(4, 5)
+			allow(airport).to receive(:weather).and_return(4,5)
 			expect(airport).to be_stormy
 		end
 
 		it "cannot land a plane when it's stormy" do 
-			allow(airport).to receive(:weather).and_return(4, 5)
+			allow(airport).to receive(:weather).and_return(4,5)
 			expect{airport.accept(plane)}.to raise_error "Stormy weather - Plane cannot land"		
 		end
 
 		it "cannot take off a plane when it's stormy" do
-			allow(airport).to receive(:weather).and_return(1, 2, 3)
+			allow(airport).to receive(:weather).and_return(1,3)
 			airport.accept(plane)
-			allow(airport).to receive(:weather).and_return(4, 5)
+			allow(airport).to receive(:weather).and_return(4,5)
 			expect{airport.release(plane)}.to raise_error "Stormy weather - Plane cannot take off"		
 		end
 	end
